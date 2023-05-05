6cb354fbcba8122fb703e7af5ddc7262321503ce
Сергей <privet15.89@mail.ru>
2023-05-05 21:11:49 +0500

    feat: set routes and feedback

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Register} from "./features/Auth/Register";
+import {Login} from "./features/Auth/Login";
+import {Header} from "./common/Header/Header";
+import {Path} from "./bll/Path";
+import {LinearProgress} from "@mui/material";
+import app from 'bll/store/app'
+import todos from 'bll/store/todolists'
+import {observer} from "mobx-react-lite";
+import {Todolist} from "./features/Todolists/Todolist";
+import {ErrorAlert} from "./common/ErrorAlert/ErrorAlert";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = observer(() => {
 
-export default App;
+    return (
+        <div className="App">
+            <Header/>
+            {app.state.status === 'loading' && <LinearProgress/>}
+            <Routes>
+                <Route path={'/'} element={<Navigate to={Path.TODOS}/>}/>
+                <Route path={Path.REG} element={<Register/>}/>
+                <Route path={Path.LOGIN} element={<Login/>}/>
+                <Route path={Path.TODOS} element={<Todolist todolist={todos.todolist}
+                                                            fetchTodo={todos.fetchTodolist}
+                                                            filter={todos.todolist.filter}
+                                                            changeTodoFilter={todos.changeTodolistFilter}/>}/>
+                <Route path={Path["404"]} element={<h1>404 Page not found</h1>}/>
+                <Route path={'*'} element={<Navigate to={Path["404"]}/>}/>
+            </Routes>
+            <ErrorAlert/>
+        </div>
+    )
+});
